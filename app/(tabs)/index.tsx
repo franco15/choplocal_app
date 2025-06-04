13fb8de77699cfd7746f7a99cabb361518acde4b
franco15 <luis_fm_95@live.com.mx>
2025-06-03 20:42:33 -0700

    delete circleci folder

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,14 +4,14 @@ import { Text, View } from "react-native";
 export default function Index() {
 	return (
 		<View className="flex-1 justify-center items-center">
-			<Text className="text-5xl">que show</Text>
+			<Text className="text-5xl">Hello</Text>
 			{/* <Link
 				href={{
 					pathname: "/restaurant/[id]",
 					params: { id: "center" },
 				}}
 			>
-				room
+				restaurant
 			</Link> */}
 			<Home width={50} height={50} fill="#000000" />
 		</View>
--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,10 +1,12 @@
-import { StyleSheet, Text, View } from "react-native";
+import { User } from "@/constants/svgs";
 import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 const profile = () => {
 	return (
-		<View>
-			<Text>profile</Text>
+		<View className="flex-1 justify-center items-center">
+			<Text className="text-5xl">Profile</Text>
+			<User width={50} height={50} fill="#000000" />
 		</View>
 	);
 };
