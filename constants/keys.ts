ccbeca4076e9742382ce6d4340e02193ac238902
franco15 <luis_fm_95@live.com.mx>
2025-08-26 13:05:33 -0700

    test api_url

--- constants/keys.ts
+++ constants/keys.ts
@@ -1,1 +1,4 @@
 export const TOKEN_KEY = "choplocal-jwt";
+export const API_URL =
+	"https://choplocal-staging-hjetbddzc8hjd8fb.westus2-01.azurewebsites.net/";
+// export const API_URL = "http://10.0.2.2:5264/";
--- lib/api/axiosInstance.ts
+++ lib/api/axiosInstance.ts
@@ -1,15 +1,11 @@
 import axios, { AxiosError, InternalAxiosRequestConfig } from "axios";
 import * as SecureStore from "expo-secure-store";
 
-import { TOKEN_KEY } from "@/constants/keys";
-import { useRouter } from "expo-router";
+import { API_URL, TOKEN_KEY } from "@/constants/keys";
 import { isNullOrWhitespace } from "../utils";
 
 let cachedToken: string | null = null;
 let onLogout = () => {};
-
-// const API_URL = "https://localhost:44339/"; ///TODO set up env variables
-const API_URL = "http://10.0.2.2:5264/"; ///TODO set up env variables
 
 const loadToken = async () => {
 	if (!cachedToken) cachedToken = await SecureStore.getItemAsync(TOKEN_KEY);
@@ -30,8 +26,6 @@ export const setOnLogout = (callback: () => void) => {
 };
 
 const useAxios = () => {
-	const router = useRouter();
-
 	const axiosInstance = axios.create({
 		baseURL: API_URL,
 		headers: {
